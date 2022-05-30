a9c7fff1adb8d8cd47df2309aead7f4ef2887c39
galalkoro <koro-galal@outlok.com>
2022-05-30 15:11:51 +0200

    deleted file

--- script.js
+++
@@ -1,8 +0,0 @@
-Array.from(document.querySelectorAll(".letter")).forEach(el => {
-   el.innerText = randomLetter();
-});
-
-function randomLetter(){
-    const alphabet = "abcdefghijklmnopqrstuvwxyz"
-    return alphabet[Math.floor(Math.random() * alphabet.length)]
-}
